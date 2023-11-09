5fa8c695fee3eb574fa3fb006e505908ea4f90ee
Sai Dinesh Bellamkonda <136730015+dineshbellamkonda12@users.noreply.github.com>
2023-11-09 14:59:13 +0000

    Added Data Visualization, Data Prediction and Custom Visualization features

--- stanapp/urls.py
+++ stanapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.visualize_data, name='visualize_data'),
+    path('predict_range/', csrf_exempt(views.predict_range), name='predict_range'),
+    path('customdata/', csrf_exempt(views.empty_view), name='custom_data'),
 ]
--- stanapp/views.py
+++ stanapp/views.py
@@ -1,10 +1,17 @@
 # views.py
 from django.shortcuts import render
 from django.db.models import Sum
+from django.http import JsonResponse
 from django.db.models.functions import TruncMonth
 from django.db.models.functions import TruncYear
 from .models import RainfallData
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import json
+import numpy as np
+import pandas as pd
+import joblib
+from datetime import datetime
 
 def visualize_data(request):
     aggregated_data = RainfallData.objects.all().values('date').annotate(total_rainfall=Sum('rainfall'))
@@ -40,3 +47,55 @@ def visualize_data(request):
     }
 
     return render(request, 'visualize_data.html', context)
+
+def train_model_and_save():
+    data = RainfallData.objects.all()
+    dates = np.array([entry.date.timestamp() for entry in data])
+    rainfalls = np.array([entry.rainfall for entry in data])
+
+    dates = dates.reshape(-1, 1)
+
+    X_train, X_test, y_train, y_test = train_test_split(dates, rainfalls, test_size=0.2, random_state=42)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train, X_test, y_test)
+
+    joblib.dump(model, 'rainfall_model.pkl')
+
+def predict_range(request):
+    if request.method == 'POST':
+        try:
+            model = joblib.load('rainfall_model.pkl')
+        except FileNotFoundError:
+            train_model_and_save()
+            model = joblib.load('rainfall_model.pkl')
+
+        start_date_str = request.POST.get('start_date', None)
+        end_date_str = request.POST.get('end_date', None)
+
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d %H:%M')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d %H:%M')
+
+        dates = pd.date_range(start=start_date, end=end_date, freq='H')
+        predictions = []
+
+        # Aggregate data by day
+        aggregated_data = {}
+        for date in dates:
+            prediction = model.predict(np.array(date.timestamp()).reshape(-1, 1))
+            date_key = date.strftime('%Y-%m-%d')
+            if date_key not in aggregated_data:
+                aggregated_data[date_key] = []
+            aggregated_data[date_key].append(prediction.tolist()[0])
+
+        for date, values in aggregated_data.items():
+            average_value = sum(values) / len(values)
+            predictions.append({'date': date, 'prediction': average_value})
+
+        return JsonResponse({'predictions': predictions})
+    else:
+        return render(request, 'prediction_template.html')
+    
+
+def empty_view(request):
+    return render(request, 'custom_data_visualize.html')
